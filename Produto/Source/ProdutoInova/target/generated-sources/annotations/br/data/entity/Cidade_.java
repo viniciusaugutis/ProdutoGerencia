f83e60ae0446789a28ffbcd726ff181693c4334a
Vinicius <vinicius_augutis@hotmail.com>
2017-11-05 10:43:34 -0200

    Exportando projeto

---
+++ Produto/Source/ProdutoInova/target/generated-sources/annotations/br/data/entity/Cidade_.java
@@ -0,0 +1,14 @@
+package br.data.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-12T07:00:47")
+@StaticMetamodel(Cidade.class)
+public class Cidade_ { 
+
+    public static volatile SingularAttribute<Cidade, Integer> codigo;
+    public static volatile SingularAttribute<Cidade, String> nome;
+
+}
